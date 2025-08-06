0ee414681e5f542e1abce0ff764ad68bd8910a23
siddhant agrawal <siddhant-agrawal01>
2025-08-06 19:15:01 +0530

    added the vercel json file

--- config/db.js
+++ config/db.js
@@ -2,11 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      connectTimeoutMS: 30000,
+      maxPoolSize: 10,
+      heartbeatFrequencyMS: 10000,
+      maxIdleTimeMS: 30000,
+    });
     console.log(`Connected to MongoDB: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
